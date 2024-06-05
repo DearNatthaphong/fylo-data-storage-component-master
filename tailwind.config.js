f03b7b895971b5e2d847785ea66db2219ba5446a
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-06 02:25:07 +0700

    feat: add mobile-design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primaryGradient1: 'hsl(6, 100%, 80%)',
+        primaryGradient2: 'hsl(335, 100%, 65%)',
+        neutralPaleBlue: 'hsl(243, 100%, 93%)',
+        neutralGrayishBlue: 'hsl(229, 7%, 55%)',
+        neutralDarkBlue: 'hsl(228, 56%, 26%)',
+        neutralVeryDarkBlue: 'hsl(229, 57%, 11%)'
+      },
+      fontFamily: {
+        raleway: ['Raleway']
+      }
+    }
+  }
+};
